1008ff8c84d3fe2e73017c9a1961389bd3b51f34
aocet <alctn33@gmail.com>
2018-12-25 23:36:11 +0300

    loginde sifre sarti kaldirildi

--- app/src/main/java/com/ali/cs491/carbuds/FundamentalActivities/LoginActivity.java
+++ app/src/main/java/com/ali/cs491/carbuds/FundamentalActivities/LoginActivity.java
@@ -232,7 +232,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private boolean isPasswordValid(String password) {
-        return password.length() > 4;
+        return true;
     }
 
     /**
